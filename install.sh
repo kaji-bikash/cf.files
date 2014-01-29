548d78157003fb0580842fc968d8fa6286ebba54
kajisaap <kajisaap@gmail.com>
2014-01-29 17:01:58 +0545

    Making Rake command aware of UTF characters

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/.cf.files" ]; then
     git clone https://github.com/kajisaap/cf.files.git "$HOME/.cf.files"
     cd "$HOME/.cf.files"
     [ "$1" == "ask" ] && export ASK="true"
-    rake install
+    LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 rake install
 else
     echo "CloudFactory dotfiles is already installed"
 fi
