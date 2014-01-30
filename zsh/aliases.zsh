777dfe8124a7e0cad1cd7bc0666068bf2e2e7007
kajisaap <kajisaap@gmail.com>
2014-01-30 12:05:58 +0545

    Some sensible git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,8 @@
 # Aliases in this file are bash and zsh compatible
 
 # Don't change. The following determines where YADR is installed.
-# yadr=$HOME/.yadr
+cfdotfiles=$HOME/.cf.files
 
-# # YADR support
-# alias yav='yadr vim-add-plugin'
-# alias ydv='yadr vim-delete-plugin'
-# alias ylv='yadr vim-list-plugin'
-# alias yup='yadr update-plugins'
-# alias yip='yadr init-plugins'
 
 # PS
 alias psa="ps aux"
@@ -28,8 +22,8 @@ alias du='du -h -d 2'
 alias lsg='ll | grep'
 
 # Alias Editing
-alias ae='vim $yadr/zsh/aliases.zsh' #alias edit
-alias ar='source $yadr/zsh/aliases.zsh'  #alias reload
+alias ae='vim $cfdotfiles/zsh/aliases.zsh' #alias edit
+alias ar='source $cfdotfiles/zsh/aliases.zsh'  #alias reload
 
 # # vim using
 # mvim --version > /dev/null 2>&1
@@ -46,61 +40,61 @@ alias ar='source $yadr/zsh/aliases.zsh'  #alias reload
 # alias zr='source ~/.zshrc'
 
 # Git Aliases
-# alias gs='git status'
-# alias gstsh='git stash'
-# alias gst='git stash'
-# alias gsp='git stash pop'
-# alias gsa='git stash apply'
-# alias gsh='git show'
-# alias gshw='git show'
-# alias gshow='git show'
-# alias gi='vim .gitignore'
-# alias gcm='git ci -m'
-# alias gcim='git ci -m'
-# alias gci='git ci'
-# alias gco='git co'
-# alias gcp='git cp'
-# alias ga='git add -A'
-# alias guns='git unstage'
-# alias gunc='git uncommit'
-# alias gm='git merge'
-# alias gms='git merge --squash'
-# alias gam='git amend --reset-author'
-# alias grv='git remote -v'
-# alias grr='git remote rm'
-# alias grad='git remote add'
-# alias gr='git rebase'
-# alias gra='git rebase --abort'
-# alias ggrc='git rebase --continue'
-# alias gbi='git rebase --interactive'
-# alias gl='git l'
-# alias glg='git l'
-# alias glog='git l'
-# alias co='git co'
-# alias gf='git fetch'
-# alias gfch='git fetch'
-# alias gd='git diff'
-# alias gb='git b'
-# alias gbd='git b -D -w'
-# alias gdc='git diff --cached -w'
-# alias gpub='grb publish'
-# alias gtr='grb track'
-# alias gpl='git pull'
-# alias gplr='git pull --rebase'
-# alias gps='git push'
-# alias gpsh='git push'
-# alias gnb='git nb' # new branch aka checkout -b
-# alias grs='git reset'
-# alias grsh='git reset --hard'
-# alias gcln='git clean'
-# alias gclndf='git clean -df'
-# alias gclndfx='git clean -dfx'
-# alias gsm='git submodule'
-# alias gsmi='git submodule init'
-# alias gsmu='git submodule update'
-# alias gt='git t'
-# alias gbg='git bisect good'
-# alias gbb='git bisect bad'
+alias gs='git status'
+alias gst='git status'
+alias gstsh='git stash'
+alias gsp='git stash pop'
+alias gsa='git stash apply'
+alias gsh='git show'
+alias gshw='git show'
+alias gshow='git show'
+alias gi='vim .gitignore'
+alias gcm='git ci -m'
+alias gcim='git ci -m'
+alias gci='git ci'
+alias gco='git co'
+alias gcp='git cp'
+alias ga='git add -A'
+alias guns='git unstage'
+alias gunc='git uncommit'
+alias gm='git merge'
+alias gms='git merge --squash'
+alias gam='git amend --reset-author'
+alias grv='git remote -v'
+alias grr='git remote rm'
+alias grad='git remote add'
+alias gr='git rebase'
+alias gra='git rebase --abort'
+alias ggrc='git rebase --continue'
+alias gbi='git rebase --interactive'
+alias gl='git l'
+alias glg='git l'
+alias glog='git l'
+alias co='git co'
+alias gf='git fetch'
+alias gfch='git fetch'
+alias gd='git diff'
+alias gb='git b'
+alias gbd='git b -D -w'
+alias gdc='git diff --cached -w'
+alias gpub='grb publish'
+alias gtr='grb track'
+alias gpl='git pull'
+alias gplr='git pull --rebase'
+alias gps='git push'
+alias gpsh='git push'
+alias gnb='git nb' # new branch aka checkout -b
+alias grs='git reset'
+alias grsh='git reset --hard'
+alias gcln='git clean'
+alias gclndf='git clean -df'
+alias gclndfx='git clean -dfx'
+alias gsm='git submodule'
+alias gsmi='git submodule init'
+alias gsmu='git submodule update'
+alias gt='git t'
+alias gbg='git bisect good'
+alias gbb='git bisect bad'
 
 # Common shell functions
 alias less='less -r'
@@ -138,9 +132,6 @@ alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 
 alias rdm='rake db:migrate'
 alias rdmr='rake db:migrate:redo'
 
-# Zeus
-alias zs='zeus server'
-alias zc='zeus console'
 
 # Rspec
 alias rs='rspec spec'
